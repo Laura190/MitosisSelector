f2490a2f40dc384240057949cde44937ce3ba67f
Laura Cooper <l.cooper.5@warwick.ac.uk>
2022-03-11 10:41:10 +0000

    Added descriptions to functions

--- MitFunc.py
+++ MitFunc.py
@@ -21,70 +21,87 @@ from tqdm import tqdm
 # User inputs
 from getpass import getpass
 
-def get_z_stack(img, c=0, t=0):
-        """
-        Convert OMERO image object to numpy array
-        Input: img  OMERO image object
-               c    number of colour channls
-               t    number of time steps
-        """
-        zct_list = [(z, c, t)
-                    for z in range(img.getSizeZ())]  # Set dimensions of image
-        pixels = img.getPrimaryPixels()
-        # Read in data one plane at a time
-        return np.array(list(pixels.getPlanes(zct_list)))
 
-def pullOMERO(username,password,server,imageId,channel):
+def get_z_stack(img, c=0, t=0):
+    """
+    Convert OMERO image object to numpy array
+    Input: img  OMERO image object
+           c    number of colour channls
+           t    number of time steps
+    """
+    # Set dimensions of image
+    zct_list = [(z, c, t) for z in range(img.getSizeZ())]
+    pixels = img.getPrimaryPixels()
+    # Read in data one plane at a time
+    return np.array(list(pixels.getPlanes(zct_list)))
+
+
+def pullOMERO(username, password, server, imageId, channel):
+    """
+    Get image pixel and metadata from OMERO.
+    Apply maximum projection in Z and in t
+    Input: username  str  OMERO username
+           password  str  User's Password
+           server    str  Address of OMERO server
+           imageId   int  OMERO ID of image to processing
+           channel   int  Index of relevant channel
+    """
     try:
-        with BlitzGateway(username, password,host=server, port='4064',secure=True) as conn:
-                message = "Connected to OMERO, processing..."
+        with BlitzGateway(username, password, host=server, port='4064', secure=True) as conn:
+            message = "Connected to OMERO, processing..."
+            print(message)
+            image = conn.getObject('Image', imageId)
+            if image is None:
+                message = "Image %s not found, process ended" % imageId
                 print(message)
-                image = conn.getObject('Image', imageId)
-                if image is None:
-                    message = "Image %s not found, process ended" % imageId
-                    print(message)
+            else:
+                sizeT = image.getSizeT()
+                sizeX = image.getSizeX()
+                sizeY = image.getSizeY()
+                p = image.getPrimaryPixels()._obj
+                scaleX = p.getPhysicalSizeX().getValue()
+                # If multiple time steps, create max projection for each time
+                if sizeT > 1:
+                    for t in tqdm(range(sizeT)):
+                        zStack = get_z_stack(image, channel, t)
+                        if t == 0:
+                            maxZPrj = np.max(zStack, axis=0)
+                        else:
+                            maxZPrj = np.dstack(
+                                [maxZPrj, np.max(zStack, axis=0)])
+                    maxPrj = np.max(maxZPrj, axis=2)
+                    np.save('maxPrj.npy', maxPrj)
+                    np.save('maxZPrj.npy', maxZPrj)
                 else:
-                    sizeT = image.getSizeT()
-                    sizeX = image.getSizeX()
-                    sizeY = image.getSizeY()
-                    p = image.getPrimaryPixels()._obj
-                    scaleX = p.getPhysicalSizeX().getValue()
-                    # If multiple time steps, create max projection for each time
-                    if sizeT > 1:
-                        for t in tqdm(range(sizeT)):
-                            zStack = get_z_stack(image, channel, t)
-                            if t == 0:
-                                maxZPrj = np.max(zStack, axis=0)
-                            else:
-                                maxZPrj = np.dstack(
-                                    [maxZPrj, np.max(zStack, axis=0)])
-                        maxPrj = np.max(maxZPrj, axis=2)
-                        np.save('maxPrj.npy', maxPrj)
-                        np.save('maxZPrj.npy', maxZPrj)
-                    else:
-                        message = "Image %s has only one time point, skipping processing" % imageId
+                    message = "Image %s has only one time point, skipping processing" % imageId
         return sizeX, sizeY, scaleX, maxPrj, maxZPrj, image
     except Exception as e:
         print(e)
 
+
 def findROIs(df, maxPrj, sizeX, sizeY, box_size):
-        # df dataframe to store results
-        thresh = threshold_yen(maxPrj)
-        bw = closing(maxPrj > thresh, square(3))
-        cleared = clear_border(bw)
-        label_image = label(cleared)
-        for region in regionprops(label_image):
-            # take regions with large enough areas
-            if region.area >= 10:  # Approx diameter of bright spots
-                # draw rectangle around segmented cells
-                y0, x0 = region.centroid
-                # Ensure numbers aren't negative
-                minr = max(0, y0-float(box_size)/2)
-                minc = max(0, x0-float(box_size)/2)
-                maxr = min(sizeY, minr + box_size)
-                maxc = min(sizeX, minc + box_size)
-                df = pd.concat([df,{'x0': int(minc), 'x1': int(maxc),'y0': int(minr), 'y1': int(maxr)}], ignore_index=True)
-        return df
+    """
+    Use a method to identify regions of interest. Create rectangles around
+    centroid, which are within the limits of the image
+    """
+    # df dataframe to store results
+    thresh = threshold_yen(maxPrj)
+    bw = closing(maxPrj > thresh, square(3))
+    cleared = clear_border(bw)
+    label_image = label(cleared)
+    for region in regionprops(label_image):
+        # take regions with large enough areas
+        if region.area >= 10:  # Approx diameter of bright spots
+            # draw rectangle around segmented cells
+            y0, x0 = region.centroid
+            # Ensure numbers aren't negative
+            minr = max(0, y0-float(box_size)/2)
+            minc = max(0, x0-float(box_size)/2)
+            maxr = min(sizeY, minr + box_size)
+            maxc = min(sizeX, minc + box_size)
+            df = pd.concat([df, {'x0': int(minc), 'x1': int(maxc), 'y0': int(
+                  minr), 'y1': int(maxr)}], ignore_index=True)
+    return df
 
 
 if __name__ == "__main__":
@@ -93,7 +110,5 @@ if __name__ == "__main__":
     server = str(input("Server:") or "camdu.warwick.ac.uk")
     imageId = int(input("Image ID:") or "1000")
     channel = int(input("Channel:") or "0")
-    results = pullOMERO(username,password,server,imageId,channel)
+    results = pullOMERO(username, password, server, imageId, channel)
     print(results)
-    
-    
